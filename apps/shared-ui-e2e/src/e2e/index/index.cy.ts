3b3d68aa8c7744e32de83452ae15b31f5e4290e2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2023-02-09 16:58:47 +0700

    fix

--- apps/shared-ui-e2e/src/e2e/index/index.cy.ts
+++ apps/shared-ui-e2e/src/e2e/index/index.cy.ts
@@ -1,7 +1,7 @@
 describe('shared-ui: Wrapper component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=wrapper--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to Wrapper!');
-    });
+  beforeEach(() => cy.visit('/iframe.html?path=/story/button--primary'));
+
+  it('should render the component', () => {
+    cy.get('button').should('contain', 'Hello World');
+  });
 });
